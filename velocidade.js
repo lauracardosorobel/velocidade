10b58b6ae557adf11a24528035d924b3c33d63ea
lauracardosorobel <113368682+lauracardosorobel@users.noreply.github.com>
2023-10-16 08:06:11 -0300

    Add files via upload

--- velocidade.js
+++ velocidade.js
@@ -3,6 +3,18 @@ const resp = document.querySelector("h3")
 
 form.addEventListener("submit", (e) => {
     e.preventDefault()
-    const Velocidade Permitida= form.invelocidadePermitida.value
-    const Velocidade do condutor= form.invelocidadedocondutor.value
+    const permitida= number (form.inVelocidadePermitida.value)
+    const condutor= number (form.inVelocidadedocondutor.value)
+    let leve= permitida + (permitida * 0.2)
 
+    if(condutor <= permitida){
+        resp.innerText= 'Situação: Sem Multa'
+
+    } else if(condutor <= leve) {
+        resp.innerText= 'Situação: Multa leve'
+
+    } else {
+        resp.innerText= 'Situação: Multo grave'
+    
+    }
+})
